e90590142ba41edcd03f27f1cd174f53a672e8d5
Rana Sami Ul Haq <115706328+ranasamiulhaq@users.noreply.github.com>
2024-05-18 22:11:23 +0500

    ( Update courses.js ) Unenrolled a student from a specific course

--- routes/courses.js
+++ routes/courses.js
@@ -5,4 +5,22 @@ router.get("/", function (req, res, next) {
   res.send("Courses");
 });
 
+router.delete('/courses/:cid/unenroll-student/:sid', async (req, res) => {
+  try {
+    const { cid, sid } = req.params;
+
+    const course = await Course.findById(cid);
+    if (!course) {
+      return res.status(404).send('Course not found');
+    }
+
+    course.students.pull(sid);
+    await course.save();
+
+    res.status(200).send('Student unenrolled successfully');
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
