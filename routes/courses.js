782e3440c3eb4a4052bd9371e9677170f9caf0c5
nimrabaig <nimrabaig28@gmail.com>
2024-05-18 22:58:54 +0500

    course enroll and withdraw routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,17 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const mongoose = require("mongoose");
+const Course = require("../models/courses.js"); // Adjust the path as necessary
+const Student = require("../models/student"); // Adjust the path as necessary
+const { addCourse } = require("../Controllers/COurses.js");
+const router = express.Router();
 //GET Routes
 router.get("/", function (req, res, next) {
   res.send("Courses");
+});
+
+// POST /head/addcourse
+router.post("/head/addcourse", async (req, res) => {
+  addCourse(req, res);
 });
 
 module.exports = router;
--- routes/student.js
+++ routes/student.js
@@ -1,32 +1,46 @@
-var express=require("express");
-var router= express.Router();
+var express = require("express");
+var router = express.Router();
+const Student = require("../models/student");
+const { withdrawCourse, enrollCourse } = require("../Controllers/student");
+
 //GET Routes
-router.get('/',function(req,res,next){
-    res.send("Student Dashboard");
+router.get("/", function (req, res, next) {
+  res.send("Student Dashboard");
 });
-router.get('/student/teachers/:id', async (req, res) => {
-    const studentId = req.params.id;
-  
-    try {
-      
-      const student = await Student.findById(studentId).populate('classes.teacher');
-  
-      if (!student) {
-        return res.status(404).json({ message: 'Student not found' });
-      }
-  
-      const teacher = student.classes[0].teacher;
-  
-      if (!teacher) {
-        return res.status(404).json({ message: 'No teacher found for enrolled classes' });
-      }
-  
-      res.json({ teacher }); 
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
+router.get("/student/teachers/:id", async (req, res) => {
+  const studentId = req.params.id;
+
+  try {
+    const student = await Student.findById(studentId).populate(
+      "classes.teacher"
+    );
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
     }
-  });
 
+    const teacher = student.classes[0].teacher;
 
-module.exports=router;
+    if (!teacher) {
+      return res
+        .status(404)
+        .json({ message: "No teacher found for enrolled classes" });
+    }
+
+    res.json({ teacher });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.delete("/withdrawcourse/:cid", async (req, res) => {
+  withdrawCourse(req, res);
+});
+
+// POST /enrollcourse/:cid
+router.post("/enrollcourse/:cid", async (req, res) => {
+  enrollCourse(req, res);
+});
+
+module.exports = router;
